367dc84407e98b870ed8ceffe6b770bdd0675c9e
dagdanmo <37124101+dagdanmo@users.noreply.github.com>
2019-09-10 13:56:16 +0200

    first ping pong

--- src/test/java/RomanConverterTest.java
+++ src/test/java/RomanConverterTest.java
@@ -28,7 +28,10 @@ public class RomanConverterTest {
                 return "I";
             }else if(number == 2){
                 return "II";
+            } else if(number == 3){
+                    return "III";
             }
+
 
         return null;
     }
