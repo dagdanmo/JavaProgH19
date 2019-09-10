c85cf5213e23fe1dd2306bdc3190cd09b4b93c58
dagdanmo <37124101+dagdanmo@users.noreply.github.com>
2019-09-10 13:48:08 +0200

    Started programming

--- src/test/java/RomanConverterTest.java
+++ src/test/java/RomanConverterTest.java
@@ -1,6 +1,37 @@
+import org.junit.Test;
+import static junit.framework.TestCase.assertEquals;
+
 public class RomanConverterTest {
 
 
-    
+    @Test
+    public void shouldReturnIWhen1(){
+        assertEquals("I", toRoman(1));
+
+    }
+
+    @Test
+    public void shouldReturnIIWhen2(){
+        assertEquals("II", toRoman(2));
+    }
+    @Test
+    public void shouldReturnIIIWhen3(){
+        assertEquals("III", toRoman(3));
+    }
+
+
+
+
+    public static String toRoman(int number){
+
+            if(number == 1){
+                return "I";
+            }else if(number == 2){
+                return "II";
+            }
+
+        return null;
+    }
+
 
 }
